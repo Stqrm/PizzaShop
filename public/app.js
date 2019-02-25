23c9fc7203c3f4fb05b440449532b0b7e217f536
Stqrm <hater2@mail.ru>
2019-02-25 15:36:43 +0300

    Update app.js product counter in cart + Index.erb

--- public/app.js
+++ public/app.js
@@ -1,17 +1,37 @@
 function something()
-    {
-        var x = window.localStorage.getItem('bbb');
+{
+    var x = window.localStorage.getItem('bbb');
         
-        x = x * 1 + 1;
-        window.localStorage.setItem('bbb', x);
+    x = x * 1 + 1;
+    window.localStorage.setItem('bbb', x);
 
-        alert(x);
-    }
+    alert(x);
+}
 
 function addToCart(id)
-    {   
-        var key = 'product_' + id;
-        var x = window.localStorage.getItem(key);
-        x = x * 1 + 1;
-        window.localStorage.setItem(key, x);
+{   
+    var key = 'product_' + id;
+    var x = window.localStorage.getItem(key);
+    x = x * 1 + 1;
+    window.localStorage.setItem(key, x);
+
+    alert('Items in your cart:' + cartGetNumberOfItems());
+}
+
+function cartGetNumberOfItems()
+{   
+    var cnt = 0;
+
+    for(var i = 0; i < window.localStorage.length; i++)
+    {
+        var key = window.localStorage.key(i);
+        var value = window.localStorage.getItem(key);
+
+        if(key.indexOf('product_') == 0)
+        {
+            cnt = cnt + value * 1;
+        }
     }
+
+    return cnt;
+}
